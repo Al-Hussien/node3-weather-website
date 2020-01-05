32ed51b44b5838a6da1e9a6904ad119ed2b84118
Alhussien <alhussiensm@gmail.com>
2020-01-05 16:43:39 +0200

    Adding data to weather details

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined);
         }
         else{
-            callback(undefined,body.daily.data[0].summary + " It is currently "+ body.currently.temperature +" degrees out. There is a "+ body.currently.precipProbability +" % chance of rain.");
+            callback(undefined,body.daily.data[0].summary + " It is currently "+ body.currently.temperature +" degrees out. There is a "+ body.currently.precipProbability +" % chance of rain. The Temprature High is: "+body.daily.data[0].temperatureHigh+" The Temprature Low is: "+body.daily.data[0].temperatureLow);
         }
     })
 }
